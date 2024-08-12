aad69d7f000538536e4e4a7e36127571448c83de
Kongpob2003 <147183114+Kongpob2003@users.noreply.github.com>
2024-08-12 14:56:31 +0700

    Create register.dart

---
+++ lib/page/register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class registerPage extends StatefulWidget {
+  const registerPage({super.key});
+
+  @override
+  State<registerPage> createState() => _registerPageState();
+}
+
+class _registerPageState extends State<registerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
