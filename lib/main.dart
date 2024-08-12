36ed50435dd8fad4603dced7f935bf40260b8565
Kongpob2003 <147183114+Kongpob2003@users.noreply.github.com>
2024-08-12 16:17:50 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LOTTO168',
-      home: loginPage(),
+      home: LoginPage(),
     );
   }
 }
--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
 
-class loginPage extends StatefulWidget {
-  const loginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<loginPage> createState() => _loginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _loginPageState extends State<loginPage> {
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email or username',
+              ),
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add your login logic here
+              },
+              child: Text('Login'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Add your signup logic here
+              },
+              child: Text('Signup'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
