f0f8f8c18a65315d516ea4ea4747a5d251841897
Avatarui <162191182+Avatarui@users.noreply.github.com>
2024-08-26 21:36:26 +0700

    Update admin.dart

--- lib/page/admin.dart
+++ lib/page/admin.dart
@@ -154,7 +154,16 @@ class _AdminPage extends State<AdminPage> {
           SizedBox(height: 16),
           ElevatedButton(
               onPressed: () {
-                // Add functionality for สุ่มออกรางวัล
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return RandomButton(
+                      onClose: () {
+                        Navigator.of(context).pop(); // ปิด Dialog
+                      },
+                    );
+                  },
+                );
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color(0xff44CEA8), // สีพื้นหลังของปุ่ม
@@ -182,9 +191,21 @@ class _AdminPage extends State<AdminPage> {
   Widget _buildResetButton() {
     return Center(
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return ResetButtonn(
+                onClose: () {
+                  Navigator.of(context).pop(); // ปิด Dialog
+                },
+              );
+            },
+          );
+        },
         style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromARGB(255, 193, 126, 126), // สีพื้นหลังของปุ่ม
+          backgroundColor:
+              const Color.fromARGB(255, 193, 126, 126), // สีพื้นหลังของปุ่ม
           foregroundColor: Colors.white, // สีของข้อความและไอคอน
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
         ),
@@ -348,3 +369,364 @@ class _AdminPage extends State<AdminPage> {
     );
   }
 }
+//============================================ S T A R T  F R O M  T H I S ================================
+class RandomButton extends StatefulWidget {
+  final VoidCallback onClose;
+
+  const RandomButton({Key? key, required this.onClose}) : super(key: key);
+
+  @override
+  _RandomButtonState createState() => _RandomButtonState();
+}
+
+class _RandomButtonState extends State<RandomButton> {
+  int _selectedCheckbox = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 300,
+              padding: const EdgeInsets.all(16.0),
+              decoration: const BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: const Text(
+                'สุ่มออกรางวัล',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                children: [
+                  Transform.scale(
+                    scale: 1.5, // ปรับขนาดของ Checkbox ที่นี่
+                    child: Checkbox(
+                      value: _selectedCheckbox == 0,
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          _selectedCheckbox = 0;
+                        });
+                      },
+                      fillColor: MaterialStateProperty.resolveWith((states) {
+                        if (states.contains(MaterialState.selected)) {
+                          return const Color.fromRGBO(
+                              70, 189, 108, 1); // สีเมื่อถูกเลือก
+                        }
+                        return const Color.fromRGBO(
+                            115, 93, 184, 1); // สีเมื่อไม่ถูกเลือก
+                      }),
+                    ),
+                  ),
+                  const Text('สุ่มจากสลากทั้งหมด'),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                children: [
+                  Transform.scale(
+                    scale: 1.5, // ปรับขนาดของ Checkbox ที่นี่
+                    child: Checkbox(
+                      value: _selectedCheckbox == 1,
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          _selectedCheckbox = 1;
+                        });
+                      },
+                      fillColor: MaterialStateProperty.resolveWith((states) {
+                        if (states.contains(MaterialState.selected)) {
+                          return const Color.fromRGBO(
+                              70, 189, 108, 1); // สีเมื่อถูกเลือก
+                        }
+                        return const Color.fromRGBO(
+                            115, 93, 184, 1); // สีเมื่อไม่ถูกเลือก
+                      }),
+                    ),
+                  ),
+                  const Text('สุ่มจากสลากที่ลูกค้าซื้อ'),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red[400], // สีพื้นหลังที่ต้องการ
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  width: 75,
+                  child: IconButton(
+                    icon:
+                        const Icon(Icons.close, color: Colors.white, size: 30),
+                    onPressed: widget.onClose,
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green[400], // สีพื้นหลังที่ต้องการ
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  width: 75,
+                  child: IconButton(
+                    icon:
+                        const Icon(Icons.check, color: Colors.white, size: 30),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PopupButton extends StatelessWidget {
+  final VoidCallback onClose;
+
+  PopupButton({required this.onClose});
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              decoration: BoxDecoration(
+                color: Color(0xFF8E44AD), // Purple color from the image
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: Text(
+                'รีเซ็ตระบบใหม่',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    'สร้างสลากจำนวน:',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "ขั้นต่ำ 100",
+                      border: UnderlineInputBorder(),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'การรีเซ็ตระบบใหม่จะทำให้ข้อมูล\nลูกค้าและสลากหายทั้งหมด\nคุณแน่ใจหรือไม่?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red, fontSize: 14),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildActionButton(
+                          Icons.close, Color(0xFFE74C3C), onClose),
+                      _buildActionButton(Icons.check, Color(0xFF2ECC71), () {
+                        // เพิ่มการกระทำเมื่อกดปุ่มยืนยันที่นี่
+                        onClose(); // ปิด popup หลังจากยืนยัน
+                      }),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionButton(
+      IconData icon, Color color, VoidCallback onPressed) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.green[400], // สีพื้นหลังที่ต้องการ
+          borderRadius:
+              BorderRadius.circular(20) // รูปทรง (ใช้ Circle เพื่อให้เป็นวงกลม)
+          // รูปทรง (ใช้ Circle เพื่อให้เป็นวงกลม)
+          ),
+      width: 75,
+      child: IconButton(
+        icon: Icon(icon, color: Colors.white),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
+class ResetButtonn extends StatelessWidget {
+  final VoidCallback onClose;
+
+  ResetButtonn({required this.onClose});
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              decoration: BoxDecoration(
+                color: Color(0xFF8E44AD), // Purple color from the image
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: Text(
+                'รีเซ็ตระบบใหม่',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    'สร้างสลากจำนวน:',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "ขั้นต่ำ 100",
+                      border: UnderlineInputBorder(),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'การรีเซ็ตระบบใหม่จะทำให้ข้อมูล\nลูกค้าและสลากหายทั้งหมด\nคุณแน่ใจหรือไม่?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red, fontSize: 14),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildActionButtonn(
+                          Icons.close, Color(0xFFE74C3C), onClose),
+                      _buildActionButtonn(Icons.check, Color(0xFF2ECC71), () {
+                        // เพิ่มการกระทำเมื่อกดปุ่มยืนยันที่นี่
+                        onClose(); // ปิด popup หลังจากยืนยัน
+                      }),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionButtonn(
+      IconData icon, Color color, VoidCallback onPressed) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.green[400], // สีพื้นหลังที่ต้องการ
+          borderRadius:
+              BorderRadius.circular(20) // รูปทรง (ใช้ Circle เพื่อให้เป็นวงกลม)
+          // รูปทรง (ใช้ Circle เพื่อให้เป็นวงกลม)
+          ),
+      width: 75,
+      child: IconButton(
+        icon: Icon(icon, color: Colors.white),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
+
+
