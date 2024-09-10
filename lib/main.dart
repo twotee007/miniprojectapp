17b7c964c753598c53ba8169037c8beb7e8dc7d2
twotee007 <rexllyz2003@gmail.com>
2024-09-10 23:46:11 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:LOTTO168/page/screenpage.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  try {
-    await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform);
-  } catch (e) {
-    print('Error initializing Firebase: $e');
-    // Handle initialization error (e.g., show an error message to the user)
-  }
-
   runApp(const MyApp());
 }
 
